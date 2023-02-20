0e7205be1028133f38fbcca4af3167552056d3b3
Metriksonas2 <erikas.goriacevskis@gmail.com>
2023-02-21 01:31:51 +0200

    Added "Tournament" entity

---
+++ src/Entity/Tournament.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TournamentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TournamentRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Tournament
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $game = null;
+
+    #[ORM\Column]
+    private ?bool $withThirdPlaceMatch = false;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $rules = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $bracketType = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct(?\DateTimeInterface $createdAt)
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getGame(): ?string
+    {
+        return $this->game;
+    }
+
+    public function setGame(string $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+
+    public function isWithThirdPlaceMatch(): ?bool
+    {
+        return $this->withThirdPlaceMatch;
+    }
+
+    public function setWithThirdPlaceMatch(bool $withThirdPlaceMatch): self
+    {
+        $this->withThirdPlaceMatch = $withThirdPlaceMatch;
+
+        return $this;
+    }
+
+    public function getRules(): ?string
+    {
+        return $this->rules;
+    }
+
+    public function setRules(?string $rules): self
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    public function getBracketType(): ?string
+    {
+        return $this->bracketType;
+    }
+
+    public function setBracketType(string $bracketType): self
+    {
+        $this->bracketType = $bracketType;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
