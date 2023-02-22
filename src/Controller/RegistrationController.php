f63a7c46fac98d16166dfc9a351a0c94bef6e232
Metriksonas2 <erikas.goriacevskis@gmail.com>
2023-02-22 15:42:33 +0200

    Adjusted RegistrationController.php temporarily

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,12 +20,12 @@ use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
-
-    public function __construct(EmailVerifier $emailVerifier)
-    {
-        $this->emailVerifier = $emailVerifier;
-    }
+//    private EmailVerifier $emailVerifier;
+//
+//    public function __construct(EmailVerifier $emailVerifier)
+//    {
+//        $this->emailVerifier = $emailVerifier;
+//    }
 
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
@@ -47,13 +47,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('admin@esportshaven.com', 'Esports Haven'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+//                (new TemplatedEmail())
+//                    ->from(new Address('admin@esportshaven.com', 'Esports Haven'))
+//                    ->to($user->getEmail())
+//                    ->subject('Please Confirm your Email')
+//                    ->htmlTemplate('registration/confirmation_email.html.twig')
+//            );
             // do anything else you need here, like send an email
 
             return $userAuthenticator->authenticateUser(
