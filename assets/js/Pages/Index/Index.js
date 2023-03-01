999324293d92205169dc847b6b149175afd8806f
Metriksonas2 <erikas.goriacevskis@gmail.com>
2023-03-02 00:53:26 +0200

    Added index page with navbar

---
+++ assets/js/Pages/Index/Index.js
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import route from "../../Services/route";
+
+import './Index.css'
+import Navbar from "@/Components/Navbar/Navbar";
+import {usePage} from "@inertiajs/inertia-react";
+
+const Index = () => {
+  const isLoggedIn = usePage().props.isLoggedIn;
+
+  return (
+    <div>
+      <Navbar isLoggedIn={isLoggedIn} />
+    </div>
+  );
+}
+
+export default Index;
