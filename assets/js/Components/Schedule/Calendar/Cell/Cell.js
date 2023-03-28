27e331efa6f2fe6c6adbbb2b3f59be6ba60fce27
Metriksonas2 <erikas.goriacevskis@gmail.com>
2023-03-29 02:41:52 +0300

    Configured "Schedule cell"

--- assets/js/Components/Schedule/Calendar/Cell/Cell.js
+++ assets/js/Components/Schedule/Calendar/Cell/Cell.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Cell = ({ day, isEmpty = false }) => {
     return (isEmpty
-        ? (<div></div>)
+            ? (<div></div>)
 
-        : (<div className="relative flex flex-col bg-white group">
+            : (<div className="relative flex flex-col bg-white group h-[171px]">
                 <span className="mx-2 my-1 text-xs font-bold">{day}</span>
-                <div className="flex flex-col px-1 py-1 overflow-auto">
+                <div className="flex flex-col px-1 py-1 overflow-auto h-full scrollbar-thin scrollbar-thumb-gray-600">
                     <button
                         className="flex items-center flex-shrink-0 h-5 px-1 text-xs hover:bg-gray-200">
                         <span className="flex-shrink-0 w-2 h-2 border border-gray-500 rounded-full"></span>
