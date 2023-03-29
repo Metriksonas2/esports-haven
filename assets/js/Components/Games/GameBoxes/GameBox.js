404bb0762f9cde210116112eaeddf7e7d08df748
Metriksonas2 <erikas.goriacevskis@gmail.com>
2023-03-29 23:40:08 +0300

    Small UI adjustments

--- assets/js/Components/Games/GameBoxes/GameBox.js
+++ assets/js/Components/Games/GameBoxes/GameBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const GameBox = ({gameName, image, alt}) => {
     return (
-        <div className='py-2 px-4 mx-2 rounded-lg bg-yellow-200 flex font-semibold'>
+        <div className='py-2 px-4 mx-2 rounded-lg bg-[#42e2b8] flex font-semibold'>
             <img className="w-6 mr-4" src={image} alt={alt}/>
             <div>{gameName}</div>
         </div>
